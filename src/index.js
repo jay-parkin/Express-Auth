052d3de8472afb0b5d1a2b6c8ca89049e4f68010
Jason Parkin <jay-parkin91@hotmail.com>
2024-11-05 21:06:02 +1100

    feat: jwt working, protected route created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// module.exports = { app }
+const { dbConnect } = require("./functions/dbFunctions.js");
 const { app } = require("./server.js");
 
-require("dotenv").config();
+// module.exports = app;
+// const app = require()
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await dbConnect();
   console.log("Server is running on port http://localhost:" + PORT);
 });
--- src/server.js
+++ src/server.js
@@ -1,12 +1,52 @@
 const express = require("express");
+const { User } = require("./models/UserModel");
+const { generateJWT, validateUserAuth } = require("./functions/jwtFunctions");
 
 const app = express();
 
 app.use(express.json());
 
+// Enable this if you want front-ends to have more freedom about how they make requests
+// eg. this is for HTML URL-encoded forms
+// app.use(express.urlencoded({extended: true}));
+
 app.get("/", (request, response) => {
   response.json({
-    message: "Hello World!",
+    message: "Hello, world!",
+  });
+});
+
+app.post("/signup", async (request, response) => {
+  // check that a username and password are provided in request.body
+  let username = request.body.username;
+  let password = request.body.password;
+
+  if (!username || !password) {
+    response.status(400).json({
+      message: "Incorrect or missing sign-up credentials provided.",
+    });
+  }
+
+  // make a user in the DB using the username and password
+  let newUser = await User.create({ username: username, password: password });
+
+  // make a JWT based on the username and userID
+  let newJwt = generateJWT(newUser.id, newUser.username);
+
+  // return the JWT
+  response.json({
+    jwt: newJwt,
+    user: {
+      id: newUser.id,
+      username: newUser.username,
+    },
+  });
+});
+
+
+app.get("/protectedRoute", validateUserAuth, (request, response) => {
+  response.json({
+    message: "You can see protected content because you're signed in!",
   });
 });
 
