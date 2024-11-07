a608d3248c356a218d9cd3ec72a7ee3a20280979
Jason Parkin <jay-parkin91@hotmail.com>
2024-11-07 20:08:46 +1100

    min changes

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-		minLength: 3,
-		trim: true
-	},
-	password: {
-		type: String,
-		required: true,
-		minLength: 8,
-		trim: true
-	}
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+    trim: true,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
 
 module.exports = {
-	User
-}
+  User,
+};
