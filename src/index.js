97a1407165dc9ddb1ddbae0e2c42487e5be82f5e
Jason Parkin <jay-parkin91@hotmail.com>
2024-11-07 20:03:29 +1100

    start command

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ require("dotenv").config();
 
 // module.exports = { app }
 const { dbConnect } = require("./functions/dbFunctions.js");
-const { app } = require("./server.js");
+const {app} = require("./server.js");
 
 // module.exports = app;
 // const app = require()
 
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
-  await dbConnect();
-  console.log("Server is running on port http://localhost:" + PORT);
+	await dbConnect();
+	console.log("Server is running on port http://localhost:" + PORT);
 });
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,24 +1,23 @@
 const mongoose = require("mongoose");
 
-// Creates the layout of the collection inside the DB
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minLenth: 3,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLenth: 8,
-    trim: true,
-  },
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		minLength: 3,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 8,
+		trim: true
+	}
 });
 
 const User = mongoose.model("User", UserSchema);
 
 module.exports = {
-  User,
-};
+	User
+}
